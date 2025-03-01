60a2497f47c22d67d31206b4d19e0e822e2e4af7
Abhishek <ak1824871@gmail.com>
2025-03-01 16:34:24 +0530

    Added path params code

--- src/main/java/com/example/test/controller/HelloWorldController.java
+++ src/main/java/com/example/test/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.test.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,10 @@ public class HelloWorldController {
     @GetMapping("/query")
     public String sayHello(@RequestParam String name) {
         return "Hello " + name + " from BridgeLabz";
+    }
+
+    @GetMapping("/params/{name}")
+    public String sayHelloPathVariable(@PathVariable String name){
+        return "Hello " + name+" from Bridgelabz";
     }
 }
