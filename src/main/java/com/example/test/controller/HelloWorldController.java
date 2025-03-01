6416c0f7ad528b33bd6bd5edd7dd0f03f1be974d
Abhishek <ak1824871@gmail.com>
2025-03-01 18:09:30 +0530

    Added post request method

--- src/main/java/com/example/test/controller/HelloWorldController.java
+++ src/main/java/com/example/test/controller/HelloWorldController.java
@@ -1,9 +1,7 @@
 package com.example.test.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import com.example.test.dto.UserDTO;
 
 @RestController
 public class HelloWorldController {
@@ -20,5 +18,10 @@ public class HelloWorldController {
     @GetMapping("/params/{name}")
     public String sayHelloPathVariable(@PathVariable String name){
         return "Hello " + name+" from Bridgelabz";
+    }
+
+    @PostMapping("/post")
+    public String sayHelloPost(@RequestBody UserDTO user){
+        return "Hello " + user.getFirstName()+" "+user.getLastName()+" from Bridgelabz";
     }
 }
