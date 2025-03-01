fbef49054e44dca17ad2dce0d9a5e601369b0d45
Abhishek <ak1824871@gmail.com>
2025-03-01 15:24:19 +0530

    Added request params code

--- src/main/java/com/example/test/controller/HelloWorldController.java
+++ src/main/java/com/example/test/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.test.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,10 @@ public class HelloWorldController {
     @GetMapping("hello")
     public String sayHello(){
         return "Hello from BridgeLabz";
+    }
+
+    @GetMapping("/query")
+    public String sayHello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
